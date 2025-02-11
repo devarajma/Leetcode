569548666dcdcc41b8cd2920158a4a4cab4232c8
Devarajan M A <97373876+Devraaj0003@users.noreply.github.com>
2025-02-11 22:11:39 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (29.90%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -4,13 +4,5 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        p,n = 1,nums[0]
-        for i in range(1,len(nums)):
-            if p==0:
-                n = nums[i]
-                p+=1
-            elif nums[i]==n:
-                p+=1
-            else:
-                p-=1
-        return n
+        nums.sort()
+        return nums[len(nums)//2]

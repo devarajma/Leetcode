15a5ceaa1b2dc945a12990312fe3a3000479b299
Devarajan M A <97373876+devarajma@users.noreply.github.com>
2025-02-21 21:02:43 +0530

    Time: 748 ms (87.72%) | Memory: 12.9 MB (81.04%) - LeetSync

---
+++ 322-coin-change/coin-change.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def coinChange(self, coins, amount):
+        """
+        :type coins: List[int]
+        :type amount: int
+        :rtype: int
+        """
+        dp = [float('inf')]*(amount+1)
+        dp[0]=0
+
+        for c in coins:
+            for i in range(c,amount+1):
+                dp[i] = min(dp[i], (dp[i - c])+1)
+        
+        return dp[amount] if dp[amount]!= float('inf') else -1
+
+        
+        

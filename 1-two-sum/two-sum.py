43f9052c20f1235319c838ed343c73105d3c6378
Devarajan M A <97373876+devarajma@users.noreply.github.com>
2025-04-09 22:02:42 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (48.97%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,14 +1,8 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        h = {}
-
-        for i, v in enumerate(nums):
-            d = target - v
-            if d in h:
-                return [h[d],i]
-            h[v] = i
+        checked={}
+        for k, v in enumerate(nums):
+            a = target-v
+            if a in checked:
+                return [checked[a], k]
+            checked[v] = k

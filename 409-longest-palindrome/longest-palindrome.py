d9b5b2f7576f2ed8f105c5d9495bcd5e7f6f9778
Devarajan M A <97373876+devarajma@users.noreply.github.com>
2025-04-10 23:33:08 +0530

    Time: 8 ms (11.53%) | Memory: 12.4 MB (53.29%) - LeetSync

---
+++ 409-longest-palindrome/longest-palindrome.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        h={} 
+        t=o=0
+        for i in s:
+            if i in h:
+                h[i]+=1
+            else:
+                h[i]=1
+        
+        for k,v in h.items():
+            print(k,v)
+            if v % 2 == 1:
+                t += 1
+        if t >1:
+            return len(s) - t+1
+        return len(s)
+        

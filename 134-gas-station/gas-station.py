77ee3a5a3a6b64e25274f0fc2dbbd68ce64a3dc0
Devarajan M A <97373876+devarajma@users.noreply.github.com>
2025-06-26 20:08:18 +0530

    Time: 19 ms (96.62%) | Memory: 18.1 MB (51.23%) - LeetSync

--- 134-gas-station/gas-station.py
+++ 134-gas-station/gas-station.py
@@ -5,12 +5,14 @@ class Solution(object):
         :type cost: List[int]
         :rtype: int
         """
-        s, trip, tank, n = 0, 0, 0, len(gas)
+        if sum(gas)<sum(cost):
+            return -1
+
+        s, tank, n = 0, 0, len(gas)
         for i in range(n):
-            trip += gas[i] - cost[i]
             tank += gas[i] - cost[i]
             if tank < 0:
                 s = i + 1
                 tank = 0
-        return s if trip >= 0 else -1
+        return s 
 

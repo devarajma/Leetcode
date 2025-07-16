46ac16a211a4f2c11b0a2887d814fd0d84f4ee16
Devarajan M A <97373876+devarajma@users.noreply.github.com>
2025-07-16 11:55:06 +0530

    Time: 145 ms (38.18%) | Memory: 12.7 MB (79.81%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -0,0 +1,39 @@
+class Solution(object):
+    def minWindow(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        
+        i = j = 0
+        left = -1
+        cnt = 0
+        cht = {}
+        formed = 0
+        m = float('inf')
+        res =(-1,0,0)
+
+        ht = Counter(t) 
+        
+        while j < len(s):
+            cht[s[j]] = cht.get(s[j], 0) + 1
+
+            if s[j] in ht and cht[s[j]] == ht[s[j]]:
+                formed += 1
+
+            while formed == len(ht):
+                if (j - i + 1) < m:
+                    m = j - i + 1
+                    left = i
+                    cnt = m
+
+                cht[s[i]] -= 1
+                if s[i] in ht and cht[s[i]] < ht[s[i]]:
+                    formed -= 1
+
+                i += 1
+
+            j += 1
+
+        return "" if left == -1 else s[left:left + cnt]

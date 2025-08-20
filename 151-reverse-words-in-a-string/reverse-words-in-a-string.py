17ea436b54ce9e0396ce767fdd22b419ca149450
Devarajan M A <97373876+devarajma@users.noreply.github.com>
2025-08-20 22:53:42 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (32.89%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.py
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.py
@@ -4,22 +4,25 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        s = s.strip()[::-1]
-        res = ""
-        j = i = 0
-        while i < len(s):
-            if s[i] == " " :
-                res += s[j:i][::-1]
+        # s = s.strip()[::-1]
+        # res = ""
+        # j = i = 0
+        # while i < len(s):
+        #     if s[i] == " " :
+        #         res += s[j:i][::-1]
 
-                while i+1 < len(s) and s[i+1] == " ":
-                    i+=1
-                j = i+1
-                res+=" "
+        #         while i+1 < len(s) and s[i+1] == " ":
+        #             i+=1
+        #         j = i+1
+        #         res+=" "
 
-            if i == len(s)-1:
-                res += s[j:][::-1]
-            i+=1
+        #     if i == len(s)-1:
+        #         res += s[j:][::-1]
+        #     i+=1
 
-        return res
+        # return res
 
+        words = s.split()
+        rev = words[::-1]
+        return ' '.join(rev)
                 

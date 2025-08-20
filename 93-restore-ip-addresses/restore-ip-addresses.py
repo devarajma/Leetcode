d0c9b96925732e41ca3caf5f5fe57eb6e191d0a7
Devarajan M A <97373876+devarajma@users.noreply.github.com>
2025-08-20 23:45:33 +0530

    Time: 11 ms (16.39%) | Memory: 12.5 MB (15.38%) - LeetSync

---
+++ 93-restore-ip-addresses/restore-ip-addresses.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def restoreIpAddresses(self, s):
+        """
+        :type s: str
+        :rtype: List[str]
+        """
+        res = []
+        if len(s) > 12 or len(s) < 4:
+            return res
+
+        def backtrack(i, dots, curIP):
+            if dots == 4 and i == len(s):
+                res.append(curIP[:-1])
+                return 
+
+            if dots > 4:
+                return
+
+            for j in range(i, min(i+3, len(s))):
+                if int(s[i:j+1]) < 256 and (i == j  or s[i] != "0"):
+                    backtrack(j+1, dots+1, curIP + s[i:j+1] + ".")
+
+        backtrack(0,0,"")
+        return res
+        

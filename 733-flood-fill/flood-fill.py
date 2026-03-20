1aa7d49a2e2fd909e43b47ff8a6712794cef0216
Devarajan M A <97373876+devarajma@users.noreply.github.com>
2026-03-20 22:20:54 +0530

    Time: 0 ms (100.00%) | Memory: 19.8 MB (37.18%) - LeetSync

--- 733-flood-fill/flood-fill.py
+++ 733-flood-fill/flood-fill.py
@@ -1,30 +1,24 @@
-from collections import deque
-class Solution(object):
-    def floodFill(self, image, sr, sc, color):
-        """
-        :type image: List[List[int]]
-        :type sr: int
-        :type sc: int
-        :type color: int
-        :rtype: List[List[int]]
-        """
-        q = deque()
-        q.append((sr,sc))
-        m, n= len(image), len(image[0])
-        old = image[sr][sc]
-        if old == color:
-            return image
-            
-        while q:
-            q_l = len(q)
-            for _ in range(q_l):
-                i,j = q.popleft()
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+
+
+        rows, cols = len(image), len(image[0])
+        iclr = image[sr][sc]
+
+        def dfs(i, j):
+            if i >= rows or j >= cols or i < 0 or j < 0 or image[i][j] != iclr:
+                return 
+            if image[i][j] == iclr:
                 image[i][j] = color
-                for a,b in [(i+1,j), (i-1,j), (i,j+1), (i,j-1)]:
-                    if 0 <= a < m and 0 <= b < n and image[a][b] == old:
-                        q.append((a,b))
+            if iclr == color:
+                return image
+            dfs(i, j+1)
+            dfs(i+1, j)
+            dfs(i, j-1)
+            dfs(i-1, j)
+
+
+        dfs(sr, sc)
+
         return image
         
-                        
-
-       

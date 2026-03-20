92abcc13283b5570fe13bb7aebff76c9d99dfd7d
Devarajan M A <97373876+devarajma@users.noreply.github.com>
2026-03-21 00:23:12 +0530

    Time: 3 ms (25.62%) | Memory: 19.3 MB (81.49%) - LeetSync

--- 172-factorial-trailing-zeroes/factorial-trailing-zeroes.py
+++ 172-factorial-trailing-zeroes/factorial-trailing-zeroes.py
@@ -1,14 +1,21 @@
-class Solution(object):
-    def trailingZeroes(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        c = 0
-        while n > 0 :
-            n //= 5
-            c+=n
-        return c
+class Solution:
+    def trailingZeroes(self, n: int) -> int:
+        # def fact(p):
+        #     if p == 1 or p == 0:
+        #         return 1
+        #     return p* fact(p-1)
 
-    
-        
+        # def counting(x):
+        #     count = 0
+        #     while x > 0:
+        #         if x%10 == 0:
+        #             count+=1
+        #         x //= 10
+        # return count  
+
+        # return counting(fact(n)) 
+        count = 0
+        while n > 0:
+            n //= 5
+            count +=n
+        return count     

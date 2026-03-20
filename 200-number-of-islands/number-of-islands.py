5c796a544efdaa42d5f7fbf087820140304fac63
Devarajan M A <97373876+devarajma@users.noreply.github.com>
2026-03-20 20:40:55 +0530

    Time: 233 ms (86.18%) | Memory: 22.2 MB (33.50%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,27 +1,24 @@
-class Solution(object):
+class Solution:
     def numIslands(self, grid):
-        """
-        :type grid: List[List[str]]
-        :rtype: int
-        """
+        rows, cols = len(grid), len(grid[0])
+        visited = [[False]*cols for _ in range(rows)]
+        count = 0
 
-        m , n = len(grid), len(grid[0])
-        island = 0
-        
-        def dfs(i,j):
-            if i < 0  or i >= m or j < 0 or j >= n or grid[i][j] != '1':
+        def dfs(i, j):
+            if i < 0 or j < 0 or i >= rows or j >= cols or visited[i][j] or grid[i][j] == '0':
                 return
-            else:
-                grid[i][j] = '0'
-                dfs(i,j+1)
-                dfs(i,j-1)
-                dfs(i+1,j)
-                dfs(i-1,j)
-
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == '1':
-                    island += 1
-                    dfs(i,j)
-        return island
+            
+            visited[i][j] = True   # FIXED
+            
+            dfs(i+1, j)
+            dfs(i-1, j)
+            dfs(i, j+1)
+            dfs(i, j-1)
         
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == '1' and not visited[i][j]:  # FIXED
+                    dfs(i, j)
+                    count += 1
+
+        return count
